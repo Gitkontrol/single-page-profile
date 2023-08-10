0cd8c24126fbca77ea299e33f0a1446d94cf01ba
Gitkontrol <cloud.ayo1@gmail.com>
2023-08-10 15:51:35 -0400

    Second commit: npm run build

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,19 @@ const fieldDataType ={
 };
 
 
-const Footer = () => {
+  const validateEmail = (email) => {
+  // Regular expression for email format validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+
+const validateText = ( input )=> {
+  return typeof input === 'string'
+}
+
+
+const Footer= ()=>  {
  
 
   const[fields, setFields] = useState({
@@ -26,27 +38,38 @@ const Footer = () => {
   });
 
 
-
-  const handleonChange = (name, value)=> {
+  const handleonChange = (fieldname, value)=> {
     setFields((prevFields)=> ({
       ...prevFields, 
-      [name]: value
+      [fieldname]: value
     }))
   };
 
-if((fieldDataType['name']) === 'text'&& validateText(fields['name']
-]))
+  
+// if (
+//   (fieldDataType['message'] === 'text' && !validateText(fields['message'])) ||
+//    (fieldDataType['email'] === 'email' && !validateEmail(fields['email'])) ||
+//    (fieldDataType['name'] === 'text') && !validateText(fields['name']))
+//     {
+//   setError({
+    
+//     name: true,
+//     email: true,
+//     message: true 
 
-const validateText = ( input )=> {
-if(typeof input === 'string') {  
-  return true;
-} else {
-  return false
-}
-}
+//   });
+// } else {
+//   setError({
+    
+//     name: false,
+//     email: false,
+//     message: false
+//   });
+// }
+
  
   return (
-    <div className='flex flex-col mt-24 px-[2rem] gap-6'>
+    <div className='flex flex-col mt-24 gap-6'>
         <h1>Contact</h1>
         <p className='mb-10'>I would love to hear about your project and how I could help. Please fill in the form, and I&aposll get back to you as soon as possible.</p>
 
@@ -56,27 +79,29 @@ if(typeof input === 'string') {
             className='relative top-9 text-white bg-green-800' 
             type='text' 
             value={fields.name}
-            onChange={(e)=> handleonChange('name', e.target.value)} />
+            onChange={(e)=> handleonChange('name', e.target.value)}
+            />
             <p className='leading-[60px] -mb-0.5 text-left pl-6'>NAME</p><hr/>
-               {/* { error.name && <div className='text-red text-xs'>Sorry, invalid format here </div>  }                                   */}
+               { error.name && <div className='text-red text-xs'>Sorry, invalid format here </div>  }                                  
           </div>
+        
           <div> 
             <input 
-            className='relative top-11 text-white bg-green-800' 
+            className='relative top-11 text-white bg-[#242424]' 
             type='email' 
             value={fields.email}
             onChange={(e)=> handleonChange('email', e.target.value)} />
             <p className='leading-[60px] mt-0.5 text-left pl-6'>EMAIL</p><hr className='hr2'/>
-              {/* { error.email && <div className='text-red text-xs'>Sorry, invalid format here </div>  }       */}
+              { error.email && <div className='text-red text-xs'>Sorry, invalid format here </div>  }      
           </div>
           <div> 
             <input 
             className='relative left-6 top-9 text-white bg-green-800' 
             type='text' 
             value={fields.message}
-            onChange={(e)=> handleonChange('email', e.target.value)} />
+            onChange={(e)=> handleonChange('message', e.target.value)} />
             <p className='leading-[70px] mb-12 -mt-2.5 text-left pl-6'>MESSAGE</p><hr className='hr3'/>
-            {/* { error.message && <div className='text-red text-xs'>Sorry, invalid format here </div>  }             */}
+            { error.message && <div className='text-red text-xs'>Sorry, invalid format here </div>  }            
           </div>       
         </div>
         <div className='flex flex-col gap-0 -mt-[23%]'>
